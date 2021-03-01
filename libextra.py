181ebf452892df13f25ee30fda2b285e45aeed15
Liam Naddell <liamnprg@gmail.com>
2021-03-01 17:18:01 -0500

    partway thru

--- libextra.py
+++ libextra.py
@@ -1,5 +1,11 @@
 def strip_nu(s: str) -> str:
     newstr = ""
+    if s.islower():
+        s.lower()
+        return s
+    elif s.isupper():
+        s.upper()
+        return s
     for c in s:
         if not c in ['0','1','2','3','4','5','6','7','8','9']:
             newstr+=c
