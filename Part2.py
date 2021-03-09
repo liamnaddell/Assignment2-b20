2c1754d9118c988eef1d195a9a36cf5b545158ae
Liam Naddell <liamnprg@gmail.com>
2021-03-09 15:16:51 -0500

    j'étais utiliser le flask sans raison

--- Part2.py
+++ Part2.py
@@ -6,6 +6,6 @@ from flask import render_template, url_for
 @app.route('/')
 def hello():
     return render_template('base.html')
-@app.route('/<page>.html')
-def normal(page=None):
-    return render_template('page'+'.html')
+#@app.route('/<page>.html')
+#def normal(page=None):
+#    return render_template('page'+'.html')
