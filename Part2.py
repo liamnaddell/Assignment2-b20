1790e21aebce9b6a9160bc7c4ef20c4eb2b0e3cf
Liam Naddell <liamnprg@gmail.com>
2021-03-09 15:14:16 -0500

    revert to previous directory structure, trick flask

--- Part2.py
+++ Part2.py
@@ -5,7 +5,7 @@ from flask import render_template, url_for
 
 @app.route('/')
 def hello():
-    return render_template('src/base.html')
+    return render_template('base.html')
 @app.route('/<page>.html')
 def normal(page=None):
-    return render_template('src/'+page+'.html')
+    return render_template('page'+'.html')
