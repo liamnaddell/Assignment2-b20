50845e8017e5ec6f87856db6b60cc56614666cdc
Liam Naddell <liamnprg@gmail.com>
2021-03-10 15:58:46 -0500

    integrate base.html and index.html

--- Part2.py
+++ Part2.py
@@ -5,7 +5,7 @@ from flask import render_template, url_for
 
 @app.route('/')
 def hello():
-    return render_template('base.html')
+    return render_template('index.html')
 @app.route('/<page>.html')
 def normal(page=None):
     return render_template(page+'.html')
